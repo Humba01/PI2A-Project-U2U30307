ce88eaffb5f2f07a2a77ab641bee8a216ed896d0
Humba01 <humbandroid@gmail.com>
2023-04-11 01:12:11 -0300

    Construindo página

--- public/logical.js
+++ public/logical.js
@@ -1,21 +1,18 @@
 // DOM manipulação
-let gancho = document.getElementById("tabuleiro")
-  ,counter = 0
-  ,corCasaTabuleiro;
-const element = document.createElement("div");
+let gancho = document.getElementById("tabuleiro"), counter = 0;
 
 for(let i = 1; i <= 8; i++) { 
-
+  
   for(let j = 1; j <= 8; j++) {
-
+    
+    let element = document.createElement("div");
     counter++;
     element.id = `${i}${j}`;
-    element.style.gridColumn = i;
-    element.style.gridRow = j / 8; 
+    element.style.gridColumn = j;
+    element.style.gridRow = i; 
     
-    corCasaTabuleiro = counter % 2;
-    if(corCasaTabuleiro == 0) { element.classList.add("layout-tabuleiro-casa-par"); }
-    if(corCasaTabuleiro == 1) { element.classList.add("layout-tabuleiro-casa-impar"); }
+    // if(counter == 0) { element.classList.add("layout-tabuleiro-casa-par"); }
+    // if(counter == 1) { element.classList.add("layout-tabuleiro-casa-impar"); }
 
     gancho.appendChild(element);
 
